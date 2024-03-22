db10b9ffa8f444ae648f7772b4fd2d10fd8d3aed
developerkh313 <developerkh313@gmail.com>
2024-03-22 02:23:39 +0000

    feat(main): chi router and middleware added

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
-	
+
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
 )
 
 func main() {
+
+	router:= chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/ping", basicHandler)
+
 	server := &http.Server{
 		Addr:    ":3001",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
@@ -19,5 +27,5 @@ func main() {
 	}
 }
 func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	w.Write([]byte("pong"))
 }
